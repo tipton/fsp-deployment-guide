d57e4103f5641ef29626028e74d3668db235adb9
Tipton Cole <tipton@tipton.com>
2016-01-15 12:43:56 -0600

    user home directory based paths

--- apt-get-update.sh
+++ apt-get-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ansible-playbook ./prod/roles/common/tasks/apt-get-update.yml \
---private-key=/Users/theorem/projects/fsp-deployment-guide/ssh_keys/cyoa.402server.com_prod_key  \
--i /Users/theorem/projects/fsp-deployment-guide/prod/hosts \
+ansible-playbook ~/projects/fsp-deployment-guide/prod/roles/common/tasks/apt-get-update.yml \
+--private-key=~/projects/fsp-deployment-guide/ssh_keys/cyoa.402server.com_prod_key  \
+-i ~/projects/fsp-deployment-guide/prod/hosts \
 -u deployer
